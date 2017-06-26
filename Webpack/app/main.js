707a3d7eee883401436c8340aa764e26ce081033
AbelWang <135161616@qq.com>
2017-06-26 17:39:34 +0800

    大作业练习

--- Webpack/app/main.js
+++ Webpack/app/main.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import {render} from 'react-dom';
+import {
+	render
+} from 'react-dom';
 import LoginPopup from './LoginPopup';
 import Login from './Login';
 import './main.css'
 import './login_popup.css';
-import './close.png';
 import './index.temp.html';
-render(<LoginPopup loginEvent={Login.post} loginEvent={Login.post}/>,document.getElementById('root'));
+import './imgs/banner1.jpg';
+import './imgs/banner2.jpg';
+import './imgs/banner3.jpg';
+// render(<LoginPopup loginEvent={Login.post} loginEvent={Login.post}/>, <document className="getE"></document>lementById('root'));
