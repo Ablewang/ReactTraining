32410be03637f003e0e51c3b253f90ae97296b6c
AbelWang <135161616@qq.com>
2017-07-03 10:46:18 +0800

    Update List's redrawing logic

--- app/script/IndexCon.js
+++ app/script/IndexCon.js
@@ -40,8 +40,8 @@ class Hot extends Component {
 }
 
 class HotMove extends Component {
-	popupMove(){
-		render(<PopupMove />,document.getElementById('popup'));
+	popupMove() {
+		render(<PopupMove />, document.getElementById('popup'));
 	}
 	render() {
 		let move = this.props.move
@@ -58,30 +58,39 @@ class HotMove extends Component {
 	}
 }
 
-class PopupMove extends Component{
-	constructor(){
+class PopupMove extends Component {
+	constructor() {
 		super()
-		this.state={
-			popup_unique:new Date().getTime(),
-			width:0,
-			height:0
+		this.state = {
+			popup_unique: new Date().getTime(),
+			width: 0,
+			height: 0
 		}
 	}
-	componentWillMount(){
+	componentWillMount() {
 		let maxHeight = document.body.clientHeight * 0.6;
 		this.setState({
-			width:maxHeight*1.57,
-			height:maxHeight
-		},()=>{console.log(this.state)})
+			width: maxHeight * 1.57,
+			height: maxHeight
+		}, () => {
+			console.log(this.state)
+		})
 	}
-	render(){
-		return (
-			<Popup shade_close={true} title="请观看下面的视频" unique={this.state.popup_unique} content={
+	render() {
+		return ( < Popup shade_close = {
+				true
+			}
+			title = "请观看下面的视频"
+			unique = {
+				this.state.popup_unique
+			}
+			content = {
 				<div className='move-continer' style={{width:this.state.width,height:this.state.height}}>
 					<div className="mv-start-squer-big"><div className="mv-start-tran-big"></div></div>
 					<img className="move-img" src="../imgs/move-big.jpg" alt="" />
 				</div>
-			}/>
+			}
+			/>
 		)
 	}
 }
@@ -155,7 +164,7 @@ class ListTab extends Component {
 			this.props.setTab(curli.innerHTML);
 		}
 	}
-	componentDidMount(){
+	componentDidMount() {
 		this.props.setTab(this.refs.tabs.children[0].innerHTML);
 	}
 	render() {
@@ -187,42 +196,50 @@ class ListTable extends Component {
 			cur_tab: '',
 			pagesize: 12,
 			cur_pg: 1,
-			max_pg: 1
+			max_pg: 1,
+			max_col: 1
 		}
 	}
-	componentWillMount(){
+	componentWillMount() {
 		this.state.cur_tab = this.props.cur_tab;
 		this.setPage(1);
+		window.onresize = () => {
+			let cur_max_col = this.getMaxColumn();
+			if (cur_max_col != this.state.max_col) {
+				this.setPage(this.state.cur_pg)
+			}
+		}
 	}
-	componentWillReceiveProps(nextProps){
+	componentWillReceiveProps(nextProps) {
 		this.state.cur_tab = nextProps.cur_tab;
 		this.setPage(1);
 	}
-	getMaxColumn(){
+	getMaxColumn() {
 		return document.body.scrollWidth > 1585 ? 4 : 3;
 	}
 	setPage(page) {
 		this.setState({
 			cur_pg: page,
-			cur_lst: action.getListRangeData(this.state.cur_tab, page, this.state.pagesize)
+			cur_lst: action.getListRangeData(this.state.cur_tab, page, this.state.pagesize),
+			max_col: this.getMaxColumn()
 		});
 	}
-	getWidthWithMargin(obj){
-		return obj.offsetWidth + (parseInt(this.getObjectStyle(obj,'marginLeft').replace('px',''))) + (parseInt(this.getObjectStyle(obj,'marginRight').replace('px','')))
+	getWidthWithMargin(obj) {
+		return obj.offsetWidth + (parseInt(this.getObjectStyle(obj, 'marginLeft').replace('px', ''))) + (parseInt(this.getObjectStyle(obj, 'marginRight').replace('px', '')))
 	}
-	getObjectStyle(obj,style){
-		return obj.currentStyle?  obj.currentStyle[style] : document.defaultView.getComputedStyle(obj,null)[style];
+	getObjectStyle(obj, style) {
+		return obj.currentStyle ? obj.currentStyle[style] : document.defaultView.getComputedStyle(obj, null)[style];
 	}
 	render() {
-		let maxColumn = this.getMaxColumn();
+		let maxColumn = this.state.max_col;
 		let first = true;
 		let item = this.state.cur_lst;
 		let tab = this.state.cur_tab;
 		let key = item.index;
-		let spaceNum = item.data.length % maxColumn ? (maxColumn - item.data.length % maxColumn):0;
-		let createSpace = (num)=>{
+		let spaceNum = item.data.length % maxColumn ? (maxColumn - item.data.length % maxColumn) : 0;
+		let createSpace = (num) => {
 			let res = [];
-			for (var i = 0; i < num ; i++) {
+			for (var i = 0; i < num; i++) {
 				res.push(<div key={"sapce"+i} className="ls-itm-space"></div>)
 			}
 			return res;
@@ -276,18 +293,18 @@ class ListPage extends Component {
 			df_mx_pg: 8
 		}
 	}
-	handleClick(e){
+	handleClick(e) {
 		let li = e.target;
 		this.resetLiStyle(li);
 		this.paging(parseInt(li.innerHTML));
 	}
-	nextPage(page){
+	nextPage(page) {
 		page += this.props.cur_pg;
 		if (page > 0 && page <= this.props.mx_pg) {
 			this.paging(page);
 		}
 	}
-	paging(page){
+	paging(page) {
 		this.props.setPage(page);
 	}
 	resetLiStyle(li) {
@@ -299,7 +316,7 @@ class ListPage extends Component {
 	}
 	render() {
 		let cur = this.props.cur_pg;
-		let max = this.state.df_mx_pg > this.props.mx_pg ?  this.props.mx_pg : this.state.df_mx_pg;
+		let max = this.state.df_mx_pg > this.props.mx_pg ? this.props.mx_pg : this.state.df_mx_pg;
 		let ls = (max) => {
 			let res = [];
 			for (let i = 1; i <= max; i++) {
